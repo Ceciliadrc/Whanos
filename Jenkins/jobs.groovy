f71625958fd36ea6dcc94596fe1b6bf955f00a59
Cecilia Deriche <cecilia.deriche@epitech.eu>
2025-10-14 19:22:22 +0200

    ADD: add start of jobs for jenkins

---
+++ Jenkins/jobs.groovy
@@ -0,0 +1,102 @@
+folder('Whanos base images') {
+    description('Whanos Base Images')
+    displayName('Whanos base images')
+}
+
+folder('Projects') {
+    description('Projects')
+    displayName('Projects')
+}
+
+job('Whanos base images/whanos-c') {
+    parameters {
+        stringParam('IMAGE_NAME_C', '', 'Image name for C')
+    }
+    steps {
+        wrappers {
+            preBuildCleanup()
+        }
+        shell('docker build -t ${IMAGE_NAME_C} -f images/c/Dockerfile.base') //TODO: check the access to the base image
+    }
+}
+
+job('Whanos base images/whanos-java') {
+    parameters {
+        stringParam('IMAGE_NAME_JAVA', '', 'Image name for Java')
+    }
+    steps {
+        wrappers {
+            preBuildCleanup()
+        }
+        shell('docker build -t ${IMAGE_NAME_JAVA} -f images/java/Dockerfile.base') //TODO: check the access to the base image
+    }
+}
+
+job('Whanos base images/whanos-javascript') {
+    parameters {
+        stringParam('IMAGE_NAME_JAVASCRIPT', '', 'Image name for Javascript')
+    }
+    steps {
+        wrappers {
+            preBuildCleanup()
+        }
+        shell('docker build -t ${IMAGE_NAME_JAVASCRIPT} -f images/javascript/Dockerfile.base') //TODO: check the access to the base image
+    }
+}
+
+job('Whanos base images/whanos-python') {
+    parameters {
+        stringParam('IMAGE_NAME_PYTHON', '', 'Image name for Python')
+    }
+    steps {
+        wrappers {
+            preBuildCleanup()
+        }
+        shell('docker build -t ${IMAGE_NAME_PYTHON} -f images/python/Dockerfile.base') //TODO: check the access to the base image
+    }
+}
+
+job('Whanos base images/whanos-befunge') {
+    parameters {
+        stringParam('IMAGE_NAME_BEFUNGE', '', 'Image name for Befunge')
+    }
+    steps {
+        wrappers {
+            preBuildCleanup()
+        }
+        shell('docker build -t ${IMAGE_NAME_BEFUNGE} -f images/befunge/Dockerfile.base') //TODO: check the access to the base image
+    }
+}
+
+job('Whanos base images/Build all base images') { //TODO: check to do to be sure
+    stage('Trigger build of all jobs') {
+        steps {
+            parallel (
+                    c: {
+                        build job: 'Whanos base images/whanos-c'
+                    },
+                    java: {
+                        build job: 'Whanos base images/whanos-java'
+                    },
+                    javascript: {
+                        build job: 'Whanos base images/whanos-javascript'
+                    },
+                    python: {
+                        build job: 'Whanos base images/whanos-python'
+                    },
+                    befunge: {
+                        build job: 'Whanos base images/whanos-befunge'
+                    }
+            )
+        }
+    }
+}
+
+job('link-project') {
+    parameters {
+        stringParam('REPO_URL', '', 'Git repository URL')
+        stringParam('PROJECT_NAME', '', 'Name for the job to create')
+    }
+    steps {}
+
+}
